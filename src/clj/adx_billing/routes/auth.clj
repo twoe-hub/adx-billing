bece56590886145a709c37fda17ab55c16b734db
adeel <adeel.ansari@b2b.com.my>
2020-09-18 19:22:25 +0800

    Replaces selmer with hiccup for login page; it's a half-baked stuff.

--- src/clj/adx_billing/routes/auth.clj
+++ src/clj/adx_billing/routes/auth.clj
@@ -6,12 +6,81 @@
    [adx-billing.middleware :as middleware]
    [cheshire.core :refer [parse-string]]
    [clj-http.client :as client]
+   [hiccup.page :as page]
+   [garden.core :refer [css]]
    [ring.util.http-response :as response]
    [ring.util.response :refer [redirect]]
+   [ring.util.anti-forgery :as util]
    ))
 
+(def announcement false)
+(defn render-ann []
+  ;; [:div {:class "column block block-announcement"}
+  ;;  [:section {:class "section"}
+  ;;   [:h1 {:class "block-header"} ]
+  ;;   [:div {:class "block-content"}
+  ;;    [:div {:class "announcement ${i > 0 ? '' : 'is-current'}", :data-id "${i}"}
+  ;;     [:div {:class "title"} "${ann.title}"]
+  ;;     [:div {:class "subtitle published-on"} "${ann.publishDate.format(&quot;dd/MM/yyyy HH:mm&quot;)}"]
+  ;;     [:div {:class "content"} "${ann.content.encodeAsRaw()}"]]
+  ;;    [:nav {:class "pagination is-centered", :role "pagination", :aria-label "announcement pagination"}
+  ;;     [:ul {:class "pagination-list"}
+  ;;      [:g:each {:var "ann", :in "${anns}", :status "i"}
+  ;;       [:li
+  ;;        [:a {:class "${i > 0 ? '' : 'is-current'}", :data-id "${i}"}
+  ;;         [:span {:class "icon is-small"}
+  ;;          [:span {:class "fa fa-circle"}]]]]]]]]]]
+  )
+
+(defn login-template [m]
+  (page/html5
+   [:head
+    [:meta {:charset "utf-8"}]
+    [:title (:title m)]
+    (for [e (:css m)] (page/include-css e))
+    ]
+   [:body
+    [:div {:class "block-main" "columns" "is-gapless"}
+     [:content {:tag "side"}
+      (when announcement (render-ann))]
+     [:div.column.block.block-main
+      [:header
+       [:div.header-brand
+        [:img {:src "/img/adxios-brand-white.svg"}]
+        [:strong "e-Billing"]]
+       [:ul.header-links
+        [:li
+         [:a {:data-toggle "modal", :data-target "#termsOfService"} "Terms of Service"]]
+        [:li
+         [:a {:data-toggle "modal", :data-target "#privacyPolicies"} "Privacy Policy"]]]]
+
+      [:section.section
+       [:div.block.block-login
+        [:h1.block-header "Welcome to e-Billing"]
+        [:div.block-content
+         [:p.auth-message "Please login with your username and password." [:br]
+          "If you do not have one, please contact "
+          [:a {:href "mailto:info@adxios.com"} "info@adxios.com"] " to register."]]]
+       [:div#content]
+       ]
+      ]]
+    (for [e (:js m)] (page/include-js e))
+    ]))
+
+
+(defn login-page []
+  (login-template {:title "Login | e-Billing"
+                   :css ["/css/start.css"
+                         "/css/views/login/login.css"]
+                   :js ["/js/app/cljs_base.js"
+                        "/js/app/adx_billing/auth/login.js"]}))
+
 (defn render-login [request]
-  (layout/render request "auth/login.html"))
+  (response/content-type
+   (response/ok (login-page))
+   "text/html; charset=utf-8")
+  ;; (layout/render request "auth/login.html")
+  )
 
 (defn logout [request]
   (do
@@ -26,5 +95,5 @@
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
-   ["/login" {:get render-login}]
+   ["/auth/login" {:get render-login}]
    ["/logout" {:get logout}]])
